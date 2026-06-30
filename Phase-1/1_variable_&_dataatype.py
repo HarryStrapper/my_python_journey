333b16a488a1aaaf695b61a3b5b8f025d61dfbdc
Harry_strapper <samarthsonavane2007@gmail.com>
2026-06-30 18:27:57 +0530

    Add files via upload

---
+++ Phase-1/1_variable_&_dataatype.py
@@ -0,0 +1,28 @@
+# Explore int, float, str, and bool. Store a name, age, and score in variables, then print them.
+
+name = "raj"
+age = 19
+score = 85.2
+
+print (type(name))
+print ("name is : ",name)
+
+print (type(age))
+print ("age is : ",age)
+
+print (type(score))
+print ("score is : ",score)
+
+
+#output
+
+'''
+
+<class 'str'>
+name is :  raj
+<class 'int'>
+age is :  19
+<class 'float'>
+score is :  85.2
+
+'''
